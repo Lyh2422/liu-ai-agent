2f508777fbdccd3e48077f057effc659751beaf4
liuyuanhao <1951381654@qq.com>
2025-10-02 23:13:52 +0800

    功能：搭建 ReAct 智能体执行框架

---
+++ src/main/java/com/lyh/liuaiagent/agent/LiuManus.java
@@ -0,0 +1,37 @@
+package com.lyh.liuaiagent.agent;
+
+import com.lyh.liuaiagent.advisor.MyLoggerAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.stereotype.Component;
+
+/**
+ * LYH的超级智能体(拥有自主规划能力，可以直接使用)
+ */
+@Component
+public class LiuManus extends ToolCallAgent {
+
+    public LiuManus(ToolCallback[] allTools, ChatModel dashscopeChatModel) {
+        super(allTools);
+        this.setName("yuManus");
+        String SYSTEM_PROMPT = """  
+                You are LiuManus, an all-capable AI assistant, aimed at solving any task presented by the user.  
+                You have various tools at your disposal that you can call upon to efficiently complete complex requests.  
+                """;
+        this.setSystemPrompt(SYSTEM_PROMPT);
+        String NEXT_STEP_PROMPT = """  
+                Based on user needs, proactively select the most appropriate tool or combination of tools.  
+                For complex tasks, you can break down the problem and use different tools step by step to solve it.  
+                After using each tool, clearly explain the execution results and suggest the next steps.  
+                If you want to stop the interaction at any point, use the `terminate` tool/function call.  
+                """;
+        this.setNextStepPrompt(NEXT_STEP_PROMPT);
+        this.setMaxSteps(20);
+        // 初始化客户端
+        ChatClient chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultAdvisors(new MyLoggerAdvisor())
+                .build();
+        this.setChatClient(chatClient);
+    }
+}
